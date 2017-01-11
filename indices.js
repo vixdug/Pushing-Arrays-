2c6e765e14666e70a62ca00cf4efe9adc9652910
Victoria <victoria.duggan@shopify.com>
2017-01-11 23:09:51 +0000

    formatting corrections

--- indices.js
+++ indices.js
@@ -1,27 +1,27 @@
 /*refactor code to return index aswell */
 
-var example = "lighthouse in the house"
+var example = "lighthouse in the house";
 var letters = new Object;
 
 // changed conditional to account for array and allow for .push
 
 function countLetters(string){
-  var splitString = string.split(" ").join("")
+  var splitString = string.split(" ").join("");
   for (var i = 0; i < splitString.length; i++) {
-    var l = splitString.charAt(i)
-      if (letters[l] === undefined) {
+    var l = splitString.charAt(i);
+    if (letters[l] === undefined); {
       letters[l] = [i]
-      }else {
-      letters[l].push(i);
-    }
+      } else {
+        letters[l].push(i);
+      }
   }
-return(letters);
+  return(letters);
 }
 
 function print(output){
-  console.log(output)
+  console.log(output);
 }
 
 
 countLetters(example);
-print(letters)
+print(letters);
